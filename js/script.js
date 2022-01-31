3566cb83e354976ab1c7b87bd45852bbe55a4317
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-31 16:54:56 +0100

    added v-for and name contact

--- js/script.js
+++ js/script.js
@@ -3,8 +3,29 @@ console.log('vue ok', Vue);
 Vue.config.devtools = true;
 
 const app = new Vue({
-    el:'#app',
-    data:{
-        
+    el: '#app',
+    data: {
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
     }
 })
