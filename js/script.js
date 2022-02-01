2dea8fd9c127ca1b0da4e0815252fec005f8a200
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-01 12:35:02 +0100

    added function selectContact

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ console.log('vue ok', Vue);
 
 /* 
 Milestone 2
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 Click sul contatto mostra la conversazione del contatto cliccato
+
 Milestone 3
-Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” 
+il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
+l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 
@@ -16,6 +20,9 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data: {
+
+        currentIndex : 0,
+
         user: {
             name: 'Nome Utente',
             avatar: '_io'
@@ -102,6 +109,16 @@ const app = new Vue({
             },
           ],
 
-        
+      
+    },
+
+    methods:{
+
+        //* SELEZIONO I CONTATTI
+        selectContact(index){
+            this.currentIndex = index;
+        },
+
     }
+
 })
